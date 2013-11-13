68d882c7c99ad9b7f82c6791c0681f935ae5f5ff
Matt McDonnell <matt@matt-mcdonnell.com>
2013-11-13 20:41:56 +0000

    Need to specify pthread when linking

--- Promise/promise.cpp
+++ Promise/promise.cpp
@@ -3,7 +3,7 @@
 #include <future>
 
 std::future<int> f(int x) {
-	return std::async(std::launch::async, [](int x) {return 2*x;}, x);
+	return std::async(std::launch::async, [](int y) {return 2*y;}, x);
 }
 
 void print(int x) {
@@ -13,12 +13,14 @@ void print(int x) {
 int main(int argc, char* argv[])
 {
   auto p = f(2);
-  print( p );
+  p.wait();
+  print( p.get() );
 
   int x=5;
   auto update = [&x](int c) { x += c; };
   auto p2 = f(3);
-  update( p2 );
+  p2.wait();
+  update( p2.get() );
   std::cout << x << std::endl;
 
   return 0;
