b878c3d4d1d49984337d9db8e6a05ec66fb55dca
mattmcd <matt@matt-mcdonnell.com>
2013-11-30 11:06:36 +0000

    Added delay to async example.

--- Async/async.cpp
+++ Async/async.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <future>
+#include <thread>
+#include <chrono>
 
 // Example from http://solarianprogrammer.com/2012/10/17/cpp-11-async-tutorial/
 
@@ -9,7 +11,9 @@ void called_from_async() {
 
 int main(int argc, char* argv[])
 {
-  std::future<void> result( std::async( called_from_async ) );
+  std::future<void> result( std::async( std::launch::async, called_from_async ) );
+
+  std::this_thread::sleep_for( std::chrono::seconds( 1 ));
 
   std::cout << "Message from main" << std::endl;
 
